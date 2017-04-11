608034a78d0e4cd6db468ad4a3e6ce5b5fbb32ef
Suttipat <suttipat.new@gmail.com>
2017-04-11 23:45:44 +0700

    change to use twitter_display collection

--- twitter-stream.php
+++ twitter-stream.php
@@ -1,22 +1,11 @@
 <?php
   $m = new MongoClient();
   $db = $m->SocialMonitor;
-  $collection = $db->twitter;
+  $collection = $db->twitter_display;
   $cursor = $collection->find();
   $cursor->next();
   for($i = 0; $i < $_GET['count']; $i++) {
     $cursor->next();
   }
   echo json_encode($cursor->current());
-  // echo '{"twitter": [';
-  // foreach ($cursor as $document) {
-  //   // echo "<div class=\"twitter-data-mongo\">";
-  //   // echo "<p class=\"username\">" . $document["username"] . "</p>";
-  //   // echo "<p class=\"screen_name\">@" . $document["screen_name"] . "</p>";
-  //   // echo "<p class=\"text\">" . $document["text"] . "</p>";
-  //   // echo "<p class=\"collected_time\">" . $document["collected_time"] . "</p>";
-  //   // echo "</div>";
-  //   echo json_encode($document) . ',';
-  // }
-  // echo ']}';
 ?>
