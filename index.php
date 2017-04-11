c7ac93a42ab31c9f517f09c7c4bfe2bef8c97c52
Suttipat <suttipat.new@gmail.com>
2017-04-11 14:19:59 +0700

    separate javascript file

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="script.js"></script>
 	</head>
 
 	<body>
@@ -95,58 +96,6 @@
 				</div>
 			</div>
 		</div>
-		<script type="text/javascript">
-		var showlist = $('#show-list');
-		var newLi =  $('#show-list:first');
-		var showlist2 = $('#show-list2');
-		var newLi2 =  $('#show-list2:first');
-		var myTimer = setInterval(autoAdd, 2000);
-		var queue = [];
-		var queue2 = [];
-		setStart();
-		function setStart(){
-			for(var i = 0;i<20;i++){
-				queue.push(i);
-				queue2.push(i);
-			}
-			for(var i =0;i<20;i++){
-				showlist2.prepend('<li class="show">'+queue2[i]+' </li>');
-				showlist.prepend('<li class="show">'+queue[i]+' </li>');
-			}
-		}
-		outoftwenty = $('#show-list >li:gt(20)');
-		outoftwenty2 = $('#show-list2 >li:gt(20)');
-		outoftwenty2.addClass("hide");
-		outoftwenty.addClass("hide");
-		//setTimeout());
-		function autoAdd(){
-			var twitter_data_box = $('div.twitter-data-mongo').first();
-			var twitter_data = twitter_data_box.children('.username').text();
-			twitter_data += "<br>" + twitter_data_box.children('.screen_name').text();
-			twitter_data += "<br>" + twitter_data_box.children('.text').text();
-			twitter_data_box.remove();
-			queue.push(twitter_data);
-			queue2.push('auto');
-			showlist2.prepend('<li>'+queue2[(queue2.length)-1]+' </li>');
-			showlist.prepend('<li>'+queue[(queue.length)-1]+' </li>');
-			var j = queue.shift();
-			var j2 = queue2.shift();
-			newLi =  $('#show-list >li:first');
-			newLi2 =  $('#show-list2 >li:first');
-
-			setTimeout(function() {
-			newLi.addClass("show");
-			newLi2.addClass("show");
-	 }, 100);
-			outoftwenty = $('#show-list >li:gt(19)');
-			outoftwenty.addClass("hide");
-			outoftwenty2 = $('#show-list2 >li:gt(19)');
-			outoftwenty2.addClass("hide");
-			$('#show-list li.hide').remove()
-			$('#show-list2 li.hide').remove()
-
-		}
-		</script>
 	</div>
 	<?php
 		$m = new MongoClient();
