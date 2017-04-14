6a2f2622aee401bfa7cd5d7234793de90a6099a8
Suttipat <suttipat.new@gmail.com>
2017-04-14 22:51:26 +0700

    sort before use

--- twitter-stream.php
+++ twitter-stream.php
@@ -2,7 +2,7 @@
   $m = new MongoClient();
   $db = $m->SocialMonitor;
   $collection = $db->twitter_display;
-  $cursor = $collection->find();
+  $cursor = $collection->find()->sort(array('collected_year'=>1, 'collected_month'=>1, 'collected_day'=>1, 'collected_time'=>1));
   $cursor->next();
   for($i = 0; $i < (int)$_GET['count']; $i++) {
     $cursor->next();
