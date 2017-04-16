a44cc6b588aa6600f7df7de9d9518250fa3cb9b5
Suttipat <suttipat.new@gmail.com>
2017-04-16 10:44:28 +0700

    update pantip

--- zxc.js
+++ zxc.js
@@ -10,38 +10,6 @@ $( document ).ready(function() {
   var queue = [];
   var queue2 = [];
   var twitter_count = 0, pantip_count = 0;
-  // outoftwenty = $('#show-list >li:gt(10)');
-  // outoftwenty2 = $('#show-list2 >li:gt(10)');
-  // outoftwenty2.addClass("hide");
-  // outoftwenty.addClass("hide");
-  //setTimeout());
-  // function autoAdd(){
-  //   var twitter_data_box = $('div.twitter-data-mongo').first();
-  //   var twitter_data = twitter_data_box.children('.username').text();
-  //   twitter_data += "<br>" + twitter_data_box.children('.screen_name').text();
-  //   twitter_data += "<br>" + twitter_data_box.children('.text').text();
-  //   twitter_data += "<br>" + twitter_data_box.children('.collected_time').text();
-  //   twitter_data_box.remove();
-  //   queue.push(twitter_data);
-  //   queue2.push('auto');
-  //   showlist2.prepend('<li>'+queue2[(queue2.length)-1]+' </li>');
-  //   showlist.prepend('<li>'+queue[(queue.length)-1]+' </li>');
-  //   var j = queue.shift();
-  //   var j2 = queue2.shift();
-  //   newLi =  $('#show-list >li:first');
-  //   newLi2 =  $('#show-list2 >li:first');
-  //
-  //   setTimeout(function() {
-  //   newLi.addClass("show");
-  //   newLi2.addClass("show");
-  // }, 100);
-  //   outoftwenty = $('#show-list >li:gt(9)');
-  //   outoftwenty.addClass("hide");
-  //   outoftwenty2 = $('#show-list2 >li:gt(9)');
-  //   outoftwenty2.addClass("hide");
-  //   $('#show-list li.hide').remove()
-  //   $('#show-list2 li.hide').remove()
-  // }
   function getTwitterData() {
     $.get('./twitter-stream.php?count=' + twitter_count, function(data) {
       // console.log(data);
@@ -73,7 +41,7 @@ $( document ).ready(function() {
       }
       var link = 'http://www.twitter.com/' + data['user_id'] + '/status/' + data['id_str'];
       queue.push(twitter_data);
-      showlist.prepend('<li>' + '<a href="' + link + '" target="_blank">' + queue[(queue.length)-1]+'</a></li>');
+      showlist.prepend('<li>' + '<a href="' + link + '" target="_blank">' + queue[(queue.length)-1] +'</a></li>');
       // showlist.prepend('<li>'+queue[(queue.length)-1]+'</li>');
       var j = queue.shift();
       newLi =  $('.show-list.twitter li:first');
@@ -93,9 +61,10 @@ $( document ).ready(function() {
       if(data === null) {
         return;
       }
-      var pantip_data = data['username'];
-      pantip_data += " (@" + data['screen_name'] + ')';
-      pantip_data += "<br>" + data['text'];
+      var pantip_data = 'Status : ' + data['status']
+      pantip_data += "<br>" + data['username'];
+      pantip_data += "<br>Topic : " + data['title'];
+      pantip_data += "<br><br>" + data['text'];
       pantip_data += "<br><br>" + data['collected_time'];
       if(pantip_count < 9) {
         pantip_count++;
@@ -115,10 +84,10 @@ $( document ).ready(function() {
           return;
         }
       }
-      // var link = 'http://www.twitter.com/' + data['user_id'] + '/status/' + data['id_str'];
+      var link = data['link'];
       queue.push(pantip_data);
-      // showlist2.prepend('<a href="' + link + '" target="_blank">' + '<li>'+queue[(queue.length)-1]+'</li></a>');
-      showlist2.prepend('<li>'+queue[(queue.length)-1]+'</li>');
+      showlist2.prepend('<li>' + '<a href="' + link + '" target="_blank">' + queue[(queue.length)-1]+'</a></li>');
+      // showlist2.prepend('<li>'+queue[(queue.length)-1]+'</li>');
       var j = queue.shift();
       newLi2 =  $('.show-list.pantip li:first');
 
